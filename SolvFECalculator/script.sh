6b1ccd9c3e517f5e1d6d4185b82a2b4397dd3bf7
Yingze Wang <ericwangyz@pku.edu.cn>
2023-09-22 22:46:41 -0700

    enable multiple trials

--- SolvFECalculator/script.sh
+++ SolvFECalculator/script.sh
@@ -12,14 +12,43 @@ cd ../
 cd nvt
 $gmx grompp -f nvt.mdp -c ../em/em.gro -p processed.top -r ../em/em.gro -o nvt.tpr -maxwarn 10
 $gmx mdrun -deffnm nvt
+if [ $? -ne 0 ]; then
+  $gmx mdrun -deffnm nvt
+fi
+if [ $? -ne 0 ]; then
+  $gmx mdrun -deffnm nvt
+fi
+if [ $? -ne 0 ]; then
+  echo "NVT Failed";
+  exit 1
+fi
 cd ../
 
 cd npt
 $gmx grompp -f npt.mdp -c ../nvt/nvt.gro -t ../nvt/nvt.cpt -p processed.top -r ../nvt/nvt.gro -o npt.tpr -maxwarn 10
 $gmx mdrun -deffnm npt
+if [ $? -ne 0 ]; then
+  $gmx mdrun -deffnm npt
+fi
+if [ $? -ne 0 ]; then
+  $gmx mdrun -deffnm npt
+fi
+if [ $? -ne 0 ]; then
+  echo "NPT Failed";
+  exit 1
+fi
 cd ../
 
 cd prod
 $gmx grompp -f prod.mdp -c ../npt/npt.gro -t ../npt/npt.cpt -p processed.top -r ../npt/npt.gro -o prod.tpr -maxwarn 10
 $gmx mdrun -deffnm prod
-cd ../
+if [ $? -ne 0 ]; then
+  $gmx mdrun -deffnm prod
+fi
+if [ $? -ne 0 ]; then
+  $gmx mdrun -deffnm prod
+fi
+if [ $? -ne 0 ]; then
+  echo "Production Failed";
+  exit 1
+fi
